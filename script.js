17f06f499b9f2c30c1b469bf3797d6b3a1dbc28b
Felek <49903400+FeleksWebs@users.noreply.github.com>
2021-09-22 18:57:39 +0100

    CarniBingo Upload

---
+++ script.js
@@ -0,0 +1,100 @@
+const ListOfWords = [
+"Top of food chain",
+"Apex predators",
+"Hitler was vegetarian",
+"Pushing lifestyle",
+  "Where do you get your protein",
+  "I dont feel bad for animals",
+  "Plants taste bad",
+  "Bad / Ethical is subjective",
+  "Vegans kill more animals",
+  "*That Vegan Teacher*",
+  "Meat is natural",
+  "Vegans being deficient",
+  "Hunt for food",
+  "Canine teeth",
+  "We are smarter than animals",
+  "You don't know what animal wants",
+  "Humane killing",
+  "Animals hunting animals",
+  "*Denial of factual evidence*",
+  "If everyone goes vegan...",
+  "What about your phone/clothes?",
+  "Animals will take over the earth ",
+  "Animals will go extinct ",
+  "I was going to be vegan but ...",
+  "Halal / Kosher",
+  "Plants feel pain",
+  "People will still eat meat",
+  "Meat taste good",
+  "Knife in throat = painless ",
+  "Bacon tho",
+  "Tribes living in jungle",
+  "Vegans eat grass",
+  "Free range ",
+  "Milk / Eggs is ok",
+  "Cultural",
+  "Mention religious god",
+  "Religious Holiday",
+  "Is it not against the Law",
+  "1 Vegan can't make a difference",
+  "Horse Riding",
+  "Shearing sheep",
+  "Honey",
+  "Farmers will lose jobs",
+  "Veganism is expensive",
+  "I am not killing anyone",
+  "Animals can't craft/build",
+  "Animals cant't consent",
+  "Vegans are weak",
+  "Cows / Pigs are not pets",
+  "Kill to control population",
+  "What about Zoo's",
+  "Someone buys me food",
+  "Feeling bad after going Vegan",
+];
+
+var usedNums = new Array(76);
+
+function setSquare(thisSquare) {
+  var currSquare = "square" + thisSquare;
+
+  document.getElementById(currSquare).innerHTML = getNewNum();
+  document
+    .getElementById(currSquare)
+    .setAttribute("onCLick", "ChangeColor(this.id)");
+}
+
+function newCard() {
+  //Starting loop through each square card
+  for (var i = 0; i < 24; i++) {
+    //<--always this code for loops. change in red
+    setSquare(i);
+  }
+}
+
+function getNewNum() {
+  const random = Math.floor(Math.random() * ListOfWords.length);
+  const NewList = ListOfWords[random];
+  ListOfWords.splice(random, 1);
+  return NewList;
+}
+
+function anotherCard() {
+  for (var i = 1; i < usedNums.length; i++) {
+    usedNums[i] = false;
+  }
+
+  newCard();
+}
+
+function ChangeColor(id) {
+  var Elm = document.getElementById(id);
+  if (Elm.style.backgroundColor == "green" && Elm.style.color == "white") {
+    Elm.style.color = "black";
+    Elm.style.backgroundColor = "";
+  } else {
+    Elm.style.color = "white";
+    Elm.style.backgroundColor = "green";
+  }
+}
